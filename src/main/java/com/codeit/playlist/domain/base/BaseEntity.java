9177e4752b3ab7e71d9164f32ad7cdc79dcb93c4
joongwon <anjoongwon517@gmail.com>
2025-11-11 16:40:13 +0900

    feat(기타): - baseEntity, updatableEntity 생성 - 패키지 설계

---
+++ src/main/java/com/codeit/playlist/domain/base/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.codeit.playlist.domain.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)  // Auditing
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @UuidGenerator
+    @Column(name = "id", unique = true, nullable = false)
+    private UUID id;
+
+    @CreatedDate
+    @Column(name = "createdAt", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/codeit/playlist/domain/base/BaseUpdatableEntity.java
@@ -0,0 +1,18 @@
+package com.codeit.playlist.domain.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)  // Auditing
+public class BaseUpdatableEntity extends BaseEntity {
+
+    @LastModifiedDate
+    @Column(name = "updatedAt", nullable = false)
+    private LocalDateTime updatedAt;
+}
