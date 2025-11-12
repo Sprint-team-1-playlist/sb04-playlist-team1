7be389af7e1b951e839b7a3f69643d8b9f20bc99
Eunhye0k <dmsgur7305@naver.com>
2025-11-12 16:37:29 +0900

    refactor(사용자 관리): Controller 의 DI 수정

--- src/main/java/com/codeit/playlist/domain/user/controller/UserController.java
+++ src/main/java/com/codeit/playlist/domain/user/controller/UserController.java
@@ -4,11 +4,16 @@ import com.codeit.playlist.domain.user.dto.data.UserDto;
 import com.codeit.playlist.domain.user.dto.request.UserCreateRequest;
 import com.codeit.playlist.domain.user.entity.User;
 import com.codeit.playlist.domain.user.service.UserService;
+import com.codeit.playlist.domain.user.service.basic.BasicUserService;
 import jakarta.validation.Valid;
+import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class UserController {
 
-  private final UserService userService;
+  private final BasicUserService userService;
 
   @PostMapping
   public ResponseEntity<UserDto> register(@Valid @RequestBody UserCreateRequest userCreateRequest) {
