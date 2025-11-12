b7bdcb5fb5e767695f04a6a8a980044a267c2a3a
joongwon <anjoongwon517@gmail.com>
2025-11-12 13:45:53 +0900

    feat(시청 세션 관리): Exception 정의

---
+++ src/main/java/com/codeit/playlist/domain/watching/exception/WatchingNotFoundException.java
@@ -0,0 +1,15 @@
+package com.codeit.playlist.domain.watching.exception;
+
+import java.util.UUID;
+
+public class WatchingNotFoundException extends WatchingException {
+    public WatchingNotFoundException() {
+        super(WatchingErrorCode.Watching_NOT_FOUND);
+    }
+
+    public static WatchingNotFoundException withId(UUID id) {
+        WatchingNotFoundException exception = new WatchingNotFoundException();
+        exception.addDetail("watchingId", id);
+        return exception;
+    }
+}
