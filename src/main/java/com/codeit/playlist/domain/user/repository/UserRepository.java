5602fbab1a3072f3d708baf8a120aa43c699541d
Eunhye0k <dmsgur7305@naver.com>
2025-11-13 16:02:57 +0900

    feat(사용자 관리): PlayListUserDetailsService 구현, UserNotFoundException 에 username 추가

--- src/main/java/com/codeit/playlist/domain/user/repository/UserRepository.java
+++ src/main/java/com/codeit/playlist/domain/user/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.codeit.playlist.domain.user.repository;
 
 import com.codeit.playlist.domain.user.entity.User;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
   boolean existsByEmail(String email);
+
+  Optional<User> findByUsername(String username);
 }
