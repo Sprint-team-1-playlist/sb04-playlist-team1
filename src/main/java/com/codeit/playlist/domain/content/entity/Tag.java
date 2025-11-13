5a99c0e43daa82da7dbdec44c29aac99cdede4db
B1uffer <135810601+B1uffer@users.noreply.github.com>
2025-11-13 14:59:22 +0900

    fix(콘텐츠 데이터 관리): 콘텐츠 데이터 등록 수정
    태그와 관련된 문제를 수정했습니다.

--- src/main/java/com/codeit/playlist/domain/content/entity/Tag.java
+++ src/main/java/com/codeit/playlist/domain/content/entity/Tag.java
@@ -8,7 +8,6 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,22 +18,18 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Tag extends BaseEntity {
     /**
      * 콘텐츠 ID
      */
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "contents", columnDefinition = "uuid")
-    private Content contentsId;
+    private Content content;
 
     /**
      * 태그명
      */
     @Column(nullable = false, length = 50)
     private String items;
-
-    public Tag(String items) {
-        this.items = items;
-    }
 }
--- src/main/java/com/codeit/playlist/domain/content/mapper/ContentMapper.java
+++ src/main/java/com/codeit/playlist/domain/content/mapper/ContentMapper.java
@@ -2,9 +2,33 @@ package com.codeit.playlist.domain.content.mapper;
 
 import com.codeit.playlist.domain.content.dto.data.ContentDto;
 import com.codeit.playlist.domain.content.entity.Content;
+import com.codeit.playlist.domain.content.entity.Tag;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ContentMapper {
-    ContentDto toDto(Content contents);
+    ContentDto toDto(Content content, List<Tag> tag);
+
+    Content toEntity(ContentDto contentDto);
+
+//    @Named("stringToList")
+//    default List<String> stringToList(String tag) {
+//        if(tag == null || tag.isBlank()) {
+//            return List.of();
+//        }
+//        return Arrays.stream(tag.split(","))
+//                .map(String::trim)
+//                .toList();
+//    }
+//
+//    @Named("listToString")
+//    default String listToString(List<String> tags) {
+//        if(tags == null || tags.isEmpty()) {
+//            return "";
+//        }
+//        return String.join(",", tags);
+//    }
+
 }
