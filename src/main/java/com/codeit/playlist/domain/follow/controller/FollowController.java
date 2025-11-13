f0869a61f6f00e2cff9b9901f46080de3417f47d
hochan <106953872+cheis11@users.noreply.github.com>
2025-11-13 11:03:28 +0900

    style(프로필 관리): 로그 수정

--- src/main/java/com/codeit/playlist/domain/follow/controller/FollowController.java
+++ src/main/java/com/codeit/playlist/domain/follow/controller/FollowController.java
@@ -25,19 +25,19 @@ public class FollowController {
 
   @PostMapping
   public ResponseEntity<FollowDto> follow(@RequestBody FollowRequest request) {
-    log.info("[Follow] 팔로우 생성 요청: {}", request.toString());
+    log.debug("[Follow] 팔로우 생성 요청: {}", request.toString());
     FollowDto createdFollow = followService.create(request);
-    log.debug("[Follow] 팔로우 생성 응답: {}", createdFollow);
+    log.info("[Follow] 팔로우 생성 응답: {}", createdFollow);
     return ResponseEntity
-        .status(HttpStatus.OK)
+        .status(HttpStatus.CREATED)
         .body(createdFollow);
   }
 
   @GetMapping("/followed-by-me")
   public ResponseEntity<Boolean> followedByMe(@RequestParam UUID followeeId) {
-    log.info("[Follow] 특정 유저를 내가 팔로우하는지 여부 조회 요청: {}", followeeId);
+    log.debug("[Follow] 특정 유저를 내가 팔로우하는지 여부 조회 요청: {}", followeeId);
     Boolean isUserFollowedByMe = followService.followedByMe(followeeId);
-    log.debug("[Follow] 특정 유저를 내가 팔로우하는지 여부 조회 응답: {}", followeeId);
+    log.info("[Follow] 특정 유저를 내가 팔로우하는지 여부 조회 응답: {}", isUserFollowedByMe);
     return ResponseEntity
         .status(HttpStatus.OK)
         .body(isUserFollowedByMe);
