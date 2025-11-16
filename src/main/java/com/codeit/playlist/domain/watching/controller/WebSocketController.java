ec88819f2063bf3b6354d0ec65c7d741d1659a2b
joongwon <anjoongwon517@gmail.com>
2025-11-16 20:48:07 +0900

    feat(실시간 같이 보기): 콘텐츠 시청 관련 웹소켓 컨트롤러

---
+++ src/main/java/com/codeit/playlist/domain/watching/controller/WebSocketController.java
@@ -0,0 +1,32 @@
+package com.codeit.playlist.domain.watching.controller;
+
+import com.codeit.playlist.domain.watching.service.basic.BasicWatchingSessionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.UUID;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+@MessageMapping("/pub/contents/{contentId}/watch")
+public class WebSocketController {
+    private final BasicWatchingSessionService watchingSessionService;
+
+    @MessageMapping("/join")
+    public void joinWatching(@DestinationVariable UUID contentId) {
+        log.debug("[실시간 같이 보기] 시청자 join 요청 시작: contentId={}", contentId);
+        watchingSessionService.join(contentId);
+        log.info("[실시간 같이 보기] 시청자 join 요청 성공: contentId={}", contentId);
+    }
+
+    @MessageMapping("/leave")
+    public void leaveWatching(@DestinationVariable UUID contentId) {
+        log.debug("[실시간 같이 보기] 시청자 leave 요청 시작: contentId={}", contentId);
+        watchingSessionService.leave(contentId);
+        log.info("[실시간 같이 보기] 시청자 leave 요청 성공: contentId={}", contentId);
+    }
+}
