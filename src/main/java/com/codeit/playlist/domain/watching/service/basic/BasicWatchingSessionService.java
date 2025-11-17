6cfd3d4562e3770f6da07d6caecc60c52f1959c2
joongwon <anjoongwon517@gmail.com>
2025-11-17 14:27:14 +0900

    refactor(실시간 같이 보기): - randomUUID대신 고정된 UUID를 반환하도록 수정 - Redis 관련 Exception 처리 - 웹소켓 브로드캐스트를 WatchingSessionPublisher에게 위임

--- src/main/java/com/codeit/playlist/domain/watching/service/basic/BasicWatchingSessionService.java
+++ src/main/java/com/codeit/playlist/domain/watching/service/basic/BasicWatchingSessionService.java
@@ -13,12 +13,12 @@ import com.codeit.playlist.domain.user.repository.UserRepository;
 import com.codeit.playlist.domain.watching.dto.data.ChangeType;
 import com.codeit.playlist.domain.watching.dto.data.WatchingSessionDto;
 import com.codeit.playlist.domain.watching.dto.response.WatchingSessionChange;
+import com.codeit.playlist.domain.watching.event.WatchingSessionPublisher;
 import com.codeit.playlist.domain.watching.exception.WatchingSessionUpdateException;
 import com.codeit.playlist.domain.watching.repository.RedisWatchingSessionRepository;
 import com.codeit.playlist.domain.watching.service.WatchingSessionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -30,7 +30,7 @@ import java.util.UUID;
 @Slf4j
 public class BasicWatchingSessionService implements WatchingSessionService {
     private final RedisWatchingSessionRepository redisWatchingSessionRepository;
-    private final SimpMessagingTemplate messagingTemplate;
+    private final WatchingSessionPublisher publisher;
 
     private final UserMapper userMapper;
     private final UserRepository userRepository;
@@ -38,21 +38,22 @@ public class BasicWatchingSessionService implements WatchingSessionService {
     private final ContentMapper contentMapper;
     private final TagRepository tagRepository;
 
+    @Override
     public void join(UUID contentId) {
         UUID userId = getCurrentUserId();
         log.debug("[실시간 같이 보기] join 시작: contentId={}, userId={}", contentId, userId);
 
-        boolean isAdded = redisWatchingSessionRepository.addWatcher(contentId, userId);
-        if (!isAdded) {
+        boolean added = redisWatchingSessionRepository.addWatcher(contentId, userId);
+        if (!added) {
             log.error("[실시간 같이 보기] Redis 저장 실패: contentId={}, userId={}", contentId, userId);
             throw new WatchingSessionUpdateException();
         }
         long watcherCount = redisWatchingSessionRepository.countWatcher(contentId);
 
         WatchingSessionDto watchingSessionDto = createWatchingSessionDto(contentId);
-        WatchingSessionChange event = createEvent(ChangeType.JOIN, watchingSessionDto, watcherCount);
+        WatchingSessionChange event = new WatchingSessionChange(ChangeType.JOIN, watchingSessionDto, watcherCount);
 
-        sendToDestination(contentId, event);
+        publisher.publish(contentId, event);
         log.info("[실시간 같이 보기] join 완료: event={}", event);
     }
 
@@ -68,26 +69,19 @@ public class BasicWatchingSessionService implements WatchingSessionService {
         }
         long watcherCount = redisWatchingSessionRepository.countWatcher(contentId);
 
-        WatchingSessionDto watchingSessionDto = createWatchingSessionDto(contentId);
-        WatchingSessionChange event = createEvent(ChangeType.LEAVE, watchingSessionDto, watcherCount);
+        try {
+            WatchingSessionDto watchingSessionDto = createWatchingSessionDto(contentId);
+            WatchingSessionChange event = new WatchingSessionChange(ChangeType.LEAVE, watchingSessionDto, watcherCount);
 
-        sendToDestination(contentId, event);
+            publisher.publish(contentId, event);
+
+        } catch (UserNotFoundException | ContentNotFoundException e) {
+            log.warn("[실시간 같이 보기] leave 이벤트 생성 실패, count({})만 브로드캐스트:{}", watcherCount, e.getMessage());
+            WatchingSessionChange event = new WatchingSessionChange(ChangeType.LEAVE, null, watcherCount); // TODO: FE 에서 null 받는지 체크
+            publisher.publish(contentId, event);
+        }
+
         log.info("[실시간 같이 보기] leave 완료: watcherCount={}", watcherCount);
-    }
-
-    private void sendToDestination(UUID contentId, WatchingSessionChange event) {
-        messagingTemplate.convertAndSend(
-                "/sub/contents/" + contentId + "/watch",
-                event
-        );
-    }
-
-    private WatchingSessionChange createEvent(ChangeType type, WatchingSessionDto watchingSessionDto, long watcherCount) {
-        return new WatchingSessionChange(
-                type,
-                watchingSessionDto,
-                watcherCount
-        );
     }
 
     private WatchingSessionDto createWatchingSessionDto(UUID contentId) {
@@ -106,6 +100,6 @@ public class BasicWatchingSessionService implements WatchingSessionService {
     }
 
     private UUID getCurrentUserId() {
-        return UUID.randomUUID(); // TODO: security 구현되면 authentication 가져와서 대체하기
+        return UUID.fromString("5a160ff2-5420-4329-b69b-65427396ebbe"); // TODO: security 구현되면 authentication 가져와서 대체하기
     }
 }
