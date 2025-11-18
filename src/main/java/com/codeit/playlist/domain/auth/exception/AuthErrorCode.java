51d3094803abd2bc6fb3c9324ab1af5ca8382c94
Eunhye0k <dmsgur7305@naver.com>
2025-11-18 13:46:57 +0900

    style(사용자 관리): AuthErrorCode 불필요한 , 제거

--- src/main/java/com/codeit/playlist/domain/auth/exception/AuthErrorCode.java
+++ src/main/java/com/codeit/playlist/domain/auth/exception/AuthErrorCode.java
@@ -12,7 +12,7 @@ public enum AuthErrorCode implements ErrorCode {
   ACCESS_DENIED_EXCEPTION(HttpStatus.FORBIDDEN.value(), "권한이 없습니다."),
   REFRESH_TOKEN_EXCEPTION(HttpStatus.UNAUTHORIZED.value(), "REFRESH 토큰이 필요합니다."),
   INVALID_OR_EXPIRED_EXCEPTION(HttpStatus.BAD_REQUEST.value(), "REFRESH 토큰이 유효하지 않거나 만료되었습니다."),
-  JWT_INTERNAL_SERVER_ERROR_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR.value(), "JWT 처리 중 오류 발생"),;
+  JWT_INTERNAL_SERVER_ERROR_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR.value(), "JWT 처리 중 오류 발생");
 
   private final int status;
   private final String message;
