ff404665bdf26010332505b2ebfc6deebb0233ea
B1uffer <135810601+B1uffer@users.noreply.github.com>
2025-11-18 19:05:53 +0900

    fix(콘텐츠 데이터 관리): The Movie Open API 수정
    컨벤션에 따라 컨트롤러에 맵핑된 Value값들과 @RequestMapping을 삭제합니다.

--- src/main/java/com/codeit/playlist/domain/content/api/controller/TheMovieApiController.java
+++ src/main/java/com/codeit/playlist/domain/content/api/controller/TheMovieApiController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -13,7 +12,6 @@ import reactor.core.publisher.Mono;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
 public class TheMovieApiController {
     private final TheMovieApiService theMovieApiService;
 
