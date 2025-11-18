4fbbaef952a29863dc4ef15c3e1ba244b39d9eb3
Eunhye0k <dmsgur7305@naver.com>
2025-11-18 11:27:23 +0900

    feat(사용자 관리): Test 코드 수정

--- src/test/java/com/codeit/playlist/user/service/BasicUserServiceTest.java
+++ src/test/java/com/codeit/playlist/user/service/BasicUserServiceTest.java
@@ -3,11 +3,14 @@ package com.codeit.playlist.user.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.codeit.playlist.domain.security.PlaylistUserDetails;
 import com.codeit.playlist.domain.security.jwt.JwtRegistry;
 import com.codeit.playlist.domain.user.dto.data.UserDto;
+import com.codeit.playlist.domain.user.dto.request.ChangePasswordRequest;
 import com.codeit.playlist.domain.user.dto.request.UserCreateRequest;
 import com.codeit.playlist.domain.user.entity.Role;
 import com.codeit.playlist.domain.user.entity.User;
@@ -15,13 +18,18 @@ import com.codeit.playlist.domain.user.mapper.UserMapper;
 import com.codeit.playlist.domain.user.repository.UserRepository;
 import com.codeit.playlist.domain.user.service.basic.BasicUserService;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,10 +45,44 @@ public class BasicUserServiceTest {
   private PasswordEncoder passwordEncoder;
 
   @InjectMocks
-  private BasicUserService BasicuserService;
+  private BasicUserService userService;
 
   @Mock
   private JwtRegistry  jwtRegistry;
+
+  @Mock
+  private Authentication authentication;
+
+  @Mock
+  private SecurityContext securityContext;
+
+  private UUID FIXED_ID;
+  private User user;
+  private UserDto dto;
+
+  @BeforeEach
+  void setUp() {
+    FIXED_ID = UUID.randomUUID();
+
+    user = new User(
+        "test@example.com",
+        "oldPassword",
+        "테스트유저",
+        null,
+        Role.USER
+    );
+
+    // SecurityContext 설정
+    dto = new UserDto(
+        FIXED_ID,
+        null,
+        user.getEmail(),
+        user.getName(),
+        user.getProfileImageUrl(),
+        user.getRole(),
+        false
+    );
+  }
 
   @Test
   @DisplayName("사용자 생성 테스트 성공")
@@ -86,7 +128,6 @@ public class BasicUserServiceTest {
     when(userMapper.toDto(any(User.class))).thenReturn(userDto);
 
     // When
-    BasicUserService userService = new BasicUserService(userRepository, passwordEncoder, userMapper, jwtRegistry);
     UserDto result = userService.registerUser(request);
 
     // Then
@@ -99,6 +140,30 @@ public class BasicUserServiceTest {
     verify(userRepository).existsByEmail(newUser.getEmail());
     verify(passwordEncoder).encode("123456789");
     verify(userRepository).save(any(User.class));
+  }
+
+  @Test
+  @DisplayName("비밀번호 변경 성공")
+  void changePasswordSuccess() {
+    // Given
+    ChangePasswordRequest request = new ChangePasswordRequest("newPassword123");
+    PlaylistUserDetails principal = new PlaylistUserDetails(dto, user.getPassword());
+
+    when(authentication.getPrincipal()).thenReturn(principal);
+    when(securityContext.getAuthentication()).thenReturn(authentication);
+
+    SecurityContextHolder.setContext(securityContext);
+
+    when(userRepository.findById(FIXED_ID)).thenReturn(Optional.of(user));
+    when(passwordEncoder.encode("newPassword123")).thenReturn("encodedPwd");
+
+    // When
+    userService.changePassword(FIXED_ID, request);
+
+    // Then
+    verify(passwordEncoder).encode("newPassword123");
+    verify(userRepository).updatedPassword(eq(FIXED_ID), eq("encodedPwd"));
+    verify(jwtRegistry).invalidateJwtInformationByUserId(FIXED_ID);
   }
 
 }
