5ae3279dc7ba16391ed54472e8579647416cde7e
ShinEunSu <94344629+Shinsu98@users.noreply.github.com>
2025-11-19 20:24:44 +0900

    style(플레레이리스트): PlaylistController 내 오타 수정

--- src/main/java/com/codeit/playlist/domain/playlist/controller/PlaylistController.java
+++ src/main/java/com/codeit/playlist/domain/playlist/controller/PlaylistController.java
@@ -42,11 +42,11 @@ public class PlaylistController {
     @PostMapping
     public ResponseEntity<PlaylistDto> create(@Valid @RequestBody PlaylistCreateRequest request,
                                               @RequestHeader(value = "owner_id", required = false) UUID ownerId) { // 임시
-        log.debug("[플레이리스트] 생성 요청: title={}, ownerId={}", request.title(), ownerId);
+        log.debug("[플레이리스트] 생성 요청: title = {}, ownerId = {}", request.title(), ownerId);
 
         PlaylistDto playlist = playlistService.createPlaylist(request, ownerId);
 
-        log.info("플레이리스트 생성 완료 - id={}", playlist.id());
+        log.info("플레이리스트 생성 완료 - id = {}", playlist.id());
 
         return ResponseEntity.status(HttpStatus.CREATED).body(playlist);
     }
@@ -56,23 +56,23 @@ public class PlaylistController {
     public ResponseEntity<PlaylistDto> update(
             @PathVariable UUID playlistId,
             @Valid @RequestBody PlaylistUpdateRequest request) {
-        log.debug("[플레이리스트] 수정 요청: id={}", playlistId);
+        log.debug("[플레이리스트] 수정 요청: id = {}", playlistId);
 
         PlaylistDto updatedPlaylist = playlistService.updatePlaylist(playlistId, request);
 
-        log.info("플레이리스트 수정 성공 - id={}", updatedPlaylist.id());
+        log.info("플레이리스트 수정 성공 - id = {}", updatedPlaylist.id());
 
         return ResponseEntity.ok(updatedPlaylist);
     }
 
-    //플레이리스트 삭제(플레이리스트 목록조회 선 구현을 위한 임시 비활성화)
+    //플레이리스트 삭제
     @DeleteMapping("/{playlistId}")
     public ResponseEntity<Void> delete(@PathVariable UUID playlistId) {
 
-        log.debug("[플레이리스트] 삭제 요청: id={}", playlistId);
+        log.debug("[플레이리스트] 삭제 요청: id = {}", playlistId);
         playlistService.deletePlaylist(playlistId);
 
-        log.info("플레이리스트 삭제 성공 - id={}", playlistId);
+        log.info("플레이리스트 삭제 성공 - id = {}", playlistId);
         return ResponseEntity.noContent().build();
     }
 
@@ -89,7 +89,7 @@ public class PlaylistController {
             @RequestParam String sortBy  //updatedAt, subscribeCount
             ) {
         log.debug("[플레이리스트] 플레이리스트 목록 조회 요청: " +
-                "keywordLike={}, ownerIdEqual={}, subscriberIdEqual={}, cursor={}, idAfter={}, limit={}, sortBy={}, sortDirection={}",
+                "keywordLike = {}, ownerIdEqual = {}, subscriberIdEqual = {}, cursor = {}, idAfter = {}, limit = {}, sortBy = {}, sortDirection = {}",
                 keywordLike, ownerIdEqual, subscriberIdEqual, cursor, idAfter, limit, sortBy, sortDirection);
 
         CursorResponsePlaylistDto playlists = playlistService.findPlaylists(
@@ -103,7 +103,7 @@ public class PlaylistController {
                 sortDirection
         );
 
-        log.info("[플레이리스트] 플레이리스트 목록 조회 완료: dataSize={}, hasNext={}, totalCount={}, nextCursor={} nextIdAfter={}",
+        log.info("[플레이리스트] 플레이리스트 목록 조회 완료: dataSize = {}, hasNext = {}, totalCount = {}, nextCursor = {} nextIdAfter = {}",
                 playlists.data().size(), playlists.hasNext(), playlists.totalCount(), playlists.nextCursor(), playlists.nextIdAfter());
 
         return ResponseEntity.ok(playlists);
@@ -112,11 +112,11 @@ public class PlaylistController {
     //플레이리스트 단건 조회
     @GetMapping("/{playlistId}")
     public ResponseEntity<PlaylistDto> getPlaylist(@PathVariable UUID playlistId) {
-        log.debug("[플레이리스트] 플레이리스트 단건 조회 시작: id={}", playlistId);
+        log.debug("[플레이리스트] 플레이리스트 단건 조회 시작: id = {}", playlistId);
 
         PlaylistDto response = playlistService.getPlaylist(playlistId);
 
-        log.info("[플레이리스트] 플레이리스트 단건 조회 성공: id={}", playlistId);
+        log.info("[플레이리스트] 플레이리스트 단건 조회 성공: id = {}", playlistId);
         return ResponseEntity.ok(response);
     }
 
@@ -124,7 +124,7 @@ public class PlaylistController {
     @PostMapping("/{playlistId}/subscription")
     public ResponseEntity<Void> playlistSubscription(@PathVariable UUID playlistId,
                                                      @RequestHeader("USER-ID") UUID subscriberId) {//subscriberId는 Security 구현시 삭제
-        log.debug("[플레이리스트] 플레이리스트 구독 : playlistId={}, userId = {}", playlistId, subscriberId);
+        log.debug("[플레이리스트] 플레이리스트 구독 : playlistId = {}, userId = {}", playlistId, subscriberId);
 
         playlistSubscriptionService.subscribe(playlistId, subscriberId);
 
@@ -135,7 +135,7 @@ public class PlaylistController {
     @DeleteMapping("/{playlistId}/subscription")
     public ResponseEntity<Void> playlistUnSubscription(@PathVariable UUID playlistId,
                                                        @RequestHeader("USER-ID") UUID subscriberId) {//subscriberId는 Security 구현시 삭제
-        log.debug("[플레이리스트] 구독 해제 요청 : playlistId={}, userId={}", playlistId, subscriberId);
+        log.debug("[플레이리스트] 구독 해제 요청 : playlistId = {}, userId = {}", playlistId, subscriberId);
 
         playlistSubscriptionService.unsubscribe(playlistId, subscriberId);
 
@@ -148,13 +148,13 @@ public class PlaylistController {
                                                      @PathVariable UUID contentId,
                                                      @AuthenticationPrincipal PlaylistUserDetails userDetails) {
 
-        log.debug("[플레이리스트] 콘텐츠 추가 시작 : playlistId={}, contentId={}, userId{}", playlistId, contentId, userDetails.getUserDto().id());
+        log.debug("[플레이리스트] 콘텐츠 추가 시작 : playlistId = {}, contentId = {}, userId = {}", playlistId, contentId, userDetails.getUserDto().id());
 
         UUID currentUserId = userDetails.getUserDto().id();
 
         playlistContentService.addContentToPlaylist(playlistId, contentId, currentUserId);
 
-        log.info("[플레이리스트] 콘텐츠 추가 완료 : playlistId={}, contentId={}, userId={}", playlistId, contentId, currentUserId);
+        log.info("[플레이리스트] 콘텐츠 추가 완료 : playlistId = {}, contentId = {}, userId = {}", playlistId, contentId, currentUserId);
 
         return ResponseEntity.noContent().build();
     }
@@ -165,14 +165,14 @@ public class PlaylistController {
                                                           @PathVariable UUID contentId,
                                                           @AuthenticationPrincipal PlaylistUserDetails userDetails) {
 
-        log.debug("[플레이리스트] 콘텐츠 삭제 시작 : playlistId={}, contentId={}, userId{}",
+        log.debug("[플레이리스트] 콘텐츠 삭제 시작 : playlistId = {}, contentId = {}, userId = {}",
                 playlistId, contentId, userDetails.getUserDto().id());
 
         UUID currentUserId = userDetails.getUserDto().id();
 
         playlistContentService.removeContentFromPlaylist(playlistId, contentId, currentUserId);
 
-        log.info("[플레이리스트] 콘텐츠 삭제 완료 : playlistId={}, contentId={}, userId={}",
+        log.info("[플레이리스트] 콘텐츠 삭제 완료 : playlistId = {}, contentId = {}, userId = {}",
                 playlistId, contentId, currentUserId);
 
         return ResponseEntity.noContent().build();
