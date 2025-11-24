0592d4dab1d781ea07ac874d3ccbee47be9a0832
Eunhye0k <dmsgur7305@naver.com>
2025-11-24 13:00:56 +0900

    fix(사용자 관리): 유효성 검증 수정

--- src/main/java/com/codeit/playlist/domain/user/dto/request/SignInRequest.java
+++ src/main/java/com/codeit/playlist/domain/user/dto/request/SignInRequest.java
@@ -3,7 +3,7 @@ package com.codeit.playlist.domain.user.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record SignInRequest(
-    @NotBlank(message = "사용자 이름은 필수입니다")
+    @NotBlank(message = "이메일은 필수입니다")
     String username,
     @NotBlank(message = "비밀번호는 필수입니다")
     String password
