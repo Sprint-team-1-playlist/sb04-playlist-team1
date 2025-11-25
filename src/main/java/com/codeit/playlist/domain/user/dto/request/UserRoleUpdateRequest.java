50c9de5cd418ef744324b779b97245b5398d70c2
Eunhye0k <dmsgur7305@naver.com>
2025-11-25 17:55:10 +0900

    fix(사용자 관리): 사용자 역할 수정 안되는 오류 수정

--- src/main/java/com/codeit/playlist/domain/user/dto/request/UserRoleUpdateRequest.java
+++ src/main/java/com/codeit/playlist/domain/user/dto/request/UserRoleUpdateRequest.java
@@ -3,6 +3,6 @@ package com.codeit.playlist.domain.user.dto.request;
 import com.codeit.playlist.domain.user.entity.Role;
 
 public record UserRoleUpdateRequest(
-    Role newRole
+    Role role
 ) {
 }
