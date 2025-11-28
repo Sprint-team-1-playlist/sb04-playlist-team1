3b68e4e27bc6304f3293f1c57414ca1507dfd49a
hochan <106953872+cheis11@users.noreply.github.com>
2025-11-28 15:27:11 +0900

    refactor(프로필 관리): 파일명 재수정

--- src/main/java/com/codeit/playlist/domain/user/service/basic/BasicUserService.java
+++ src/main/java/com/codeit/playlist/domain/user/service/basic/BasicUserService.java
@@ -269,7 +269,7 @@ public class BasicUserService implements UserService {
       }
 
       String extension = contentType.equals("image/png") ? ".png" : ".jpg";
-      String key = "profiles/" + UUID.randomUUID() + "_" + image.getOriginalFilename() + extension;
+      String key = "profile/" + UUID.randomUUID() + "_" + image.getOriginalFilename() + extension;
 
       String imageUrl = s3Uploader.upload(
           s3Properties.getProfileBucket(),
