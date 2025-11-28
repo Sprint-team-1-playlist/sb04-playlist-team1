2f3a208bcbc2250f9ee7dfa067eeda1ab6cdd1ba
Eunhye0k <dmsgur7305@naver.com>
2025-11-28 23:31:11 +0900

    fix(사용자 관리): retry설정

--- src/main/java/com/codeit/playlist/domain/user/event/TempPasswordEventHandler.java
+++ src/main/java/com/codeit/playlist/domain/user/event/TempPasswordEventHandler.java
@@ -4,8 +4,8 @@ import com.codeit.playlist.domain.auth.service.EmailService;
 import com.codeit.playlist.domain.user.dto.data.TempPasswordIssuedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.MailException;
 import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -20,7 +20,7 @@ public class TempPasswordEventHandler {
   private final EmailService emailService;
 
   @Retryable(
-      retryFor = MailException.class,
+      retryFor = Exception.class,
       maxAttempts = 3,
       backoff = @Backoff(delay = 2000, multiplier = 2)
   )
@@ -30,6 +30,13 @@ public class TempPasswordEventHandler {
       phase = TransactionPhase.AFTER_COMMIT
   )
   public void handle(TempPasswordIssuedEvent event) {
+    log.info("📩 이메일 발송 핸들러 실행 → {}", event.email());
     emailService.sendTemporaryPassword(event.email(), event.tempPassword());
   }
+
+  @Recover
+  public void recover(Exception e, TempPasswordIssuedEvent event) {
+    log.error("❌ 이메일 발송 실패 → {} / 3회 재시도 후 포기", event.email(), e);
+  }
 }
+
