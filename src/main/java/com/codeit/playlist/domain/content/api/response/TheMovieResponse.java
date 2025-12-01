d7f248bba1b5d95f3dc77ddf55fd43943a739b3f
B1uffer <135810601+B1uffer@users.noreply.github.com>
2025-12-01 18:10:27 +0900

    feat(콘텐츠 데이터 관리): 커서 페이지네이션을 통한 콘텐츠 데이터 조회 구현
    API로부터 전달받은 데이터를 콘텐츠 데이터 형태로 저장하여 커서 페이지네이션을 통해 조회할 수 있는 기능을 추가합니다.

--- src/main/java/com/codeit/playlist/domain/content/api/response/TheMovieResponse.java
+++ src/main/java/com/codeit/playlist/domain/content/api/response/TheMovieResponse.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record TheMovieResponse(
         @JsonProperty("title") String title,
         @JsonProperty("overview") String description,
-        @JsonProperty("poster_path") String thumbnailUrl,
+        @JsonProperty("backdrop_path") String thumbnailUrl,
         @JsonProperty("vote_average") Double averageRating
 ) {
 }
--- src/main/java/com/codeit/playlist/domain/content/dto/response/CursorResponseContentDto.java
+++ src/main/java/com/codeit/playlist/domain/content/dto/response/CursorResponseContentDto.java
@@ -1,16 +1,16 @@
 package com.codeit.playlist.domain.content.dto.response;
 
-import com.codeit.playlist.domain.base.SortDirection;
+import com.codeit.playlist.domain.content.dto.data.ContentDto;
 
 import java.util.List;
 
 public record CursorResponseContentDto(
-        List<Object> data,
-        String nextCursor,
-        String nextIdAfter,
-        Boolean hasNext,
-        Integer totalCount,
-        String sortBy,
-        SortDirection sortDirection
+        List<ContentDto> data, // 데이터 목록, 콘텐츠
+        String nextCursor, // 다음 커서
+        String nextIdAfter, // 다음 요청의 보조 커서
+        Boolean hasNext, // 다음 데이터가 있는지 여부
+        Integer totalCount, // 총 데이터 개수
+        String sortBy, // 정렬 기준
+        String sortDirection // 정렬 방향
 ) {
 }
