b2810d044743a9f877d90e77a7f681190ae11a96
B1uffer <135810601+B1uffer@users.noreply.github.com>
2025-12-03 17:50:16 +0900

    style(콘텐츠 데이터 관리): TagRepository에서 사용하지 않는, 중복되는 부분을 삭제합니다.

--- src/main/java/com/codeit/playlist/domain/content/repository/TagRepository.java
+++ src/main/java/com/codeit/playlist/domain/content/repository/TagRepository.java
@@ -1,6 +1,5 @@
 package com.codeit.playlist.domain.content.repository;
 
-import com.codeit.playlist.domain.content.entity.Content;
 import com.codeit.playlist.domain.content.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,6 +10,4 @@ public interface TagRepository extends JpaRepository<Tag, UUID> {
     List<Tag> findByContentId(UUID contentId);
     List<Tag> findByContentIdIn(List<UUID> contentIds);
     void deleteAllByContentId(UUID contentId);
-
-    List<Tag> findAllByContent(Content content);
 }
