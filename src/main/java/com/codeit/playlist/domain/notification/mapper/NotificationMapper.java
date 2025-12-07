a3f10b4b030854d00660da6c59efd1fff4eb8b8c
ShinEunSu <94344629+Shinsu98@users.noreply.github.com>
2025-12-07 22:46:40 +0900

    fix(알림): 알림 저장 후 조회시 응답이 나가는 시점에서 한국 시간대로 변경이 안되는 문제 해결

--- src/main/java/com/codeit/playlist/domain/notification/mapper/NotificationMapper.java
+++ src/main/java/com/codeit/playlist/domain/notification/mapper/NotificationMapper.java
@@ -4,14 +4,31 @@ import com.codeit.playlist.domain.notification.dto.data.NotificationDto;
 import com.codeit.playlist.domain.notification.entity.Notification;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface NotificationMapper {
 
     @Mapping(target = "receiverId", source = "receiver.id")
+    @Mapping(target = "createdAt", qualifiedByName = "utcToKst")
     NotificationDto toDto(Notification notification);
 
     List<NotificationDto> toDtoList(List<Notification> notifications);
+
+    @Named("utcToKst")
+    default LocalDateTime utcToKst(LocalDateTime utc) {
+        if (utc == null) {
+            return null;
+        }
+
+        return utc
+                .atOffset(ZoneOffset.UTC)
+                .atZoneSameInstant(ZoneId.of("Asia/Seoul"))
+                .toLocalDateTime();
+    }
 }
