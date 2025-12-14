65c2ef0a5ab73aa9ced8c3eee1a7e5d5cf3d5bdf
B1uffer <135810601+B1uffer@users.noreply.github.com>
2025-12-15 01:44:30 +0900

    refactor(콘텐츠 데이터 관리): Sport 스케줄러를 삭제하고, Tasklet을 추가합니다.

--- src/main/java/com/codeit/playlist/domain/content/batch/SportContentTasklet.java
+++ src/main/java/com/codeit/playlist/domain/content/batch/SportContentTasklet.java
@@ -1,6 +1,6 @@
 package com.codeit.playlist.domain.content.batch;
 
-import com.codeit.playlist.domain.content.api.service.TheSportApiService;
+import com.codeit.playlist.domain.content.api.handler.TheSportHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
@@ -9,15 +9,19 @@ import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.stereotype.Component;
 
+import java.time.YearMonth;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class SportContentTasklet implements Tasklet {
-    private final TheSportApiService theSportApiService;
+    private final TheSportHandler theSportHandler;
 
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
         log.info("[콘텐츠 데이터 관리] The Sport 배치 수집 시작");
-        return null;
+        YearMonth lastYearMonth = YearMonth.now().minusMonths(1);
+        theSportHandler.save(lastYearMonth.getYear(), lastYearMonth.getMonthValue());
+        return RepeatStatus.FINISHED;
     }
 }
