807788c88b5e1ddf787381de141fd028f144797e
B1uffer <135810601+B1uffer@users.noreply.github.com>
2025-12-15 00:35:45 +0900

    refactor(콘텐츠 데이터 관리): 잘못된 주석을 삭제합니다.

--- src/main/java/com/codeit/playlist/domain/content/entity/Content.java
+++ src/main/java/com/codeit/playlist/domain/content/entity/Content.java
@@ -35,64 +35,7 @@ public class Content extends BaseUpdatableEntity {
     private String title;
 
     /**
-     * 컨텐츠 설명@Slf4j
-     * @Service
-     * @RequiredArgsConstructor
-     * public class TheSportApiService {
-     *     private final WebClient webClient;
-     *
-     *     private Mono<TheSportListResponse> callTheSportApi(int leagueId, String season) {
-     *         log.info("[콘텐츠 데이터 관리] TheSport API Mono 빌드 시작");
-     *         return webClient.get()
-     *                 .uri(uriBuilder -> uriBuilder
-     *                         .scheme("https")
-     *                         .host("www.thesportsdb.com")
-     *                         .path("/api/v1/json/123/eventsseason.php")
-     *                         .queryParam("id", leagueId)
-     *                         .queryParam("s", season)
-     *                         .build())
-     *                 .retrieve()
-     *                 .onStatus(s -> s.value() == 429, clientResponse -> clientResponse.createException().flatMap(Mono::error))
-     *                 .bodyToMono(TheSportListResponse.class)
-     *                 .retryWhen(
-     *                         Retry.backoff(5, Duration.ofSeconds(5))
-     *                                 .maxBackoff(Duration.ofMinutes(1))
-     *                                 .filter(exception -> exception instanceof WebClientResponseException webException && webException.getStatusCode() == HttpStatus.TOO_MANY_REQUESTS)
-     *                                 .transientErrors(true)
-     *                 )
-     *                 .doOnNext(response -> log.info("season : {}, totalEvents : {}", season, response.events() == null ? 0 : response.events().size()))
-     *                 .doOnError(WebClientResponseException.class,
-     *                         e -> log.error("[콘텐츠 데이터 관리] TheSports API Mono 빌드 에러 발생", e));
-     *     }
-     *
-     *     private Flux<TheSportResponse> fluxingTheSportApi(YearMonth yearMonth) {
-     *         log.info("[콘텐츠 데이터 관리] TheSport API Flux 빌드 시작");
-     *         int leagueId = 4328;
-     *         String season = circulateSoccerSeason(yearMonth);
-     *         return callTheSportApi(leagueId, season)
-     *                 .flatMapMany(theSportResponse -> {
-     *                     if(theSportResponse.events() == null || theSportResponse.events().isEmpty()) {
-     *                         return Flux.empty();
-     *                     }
-     *                     return Flux.fromIterable(theSportResponse.events());
-     *                         })
-     *                 .filter(response -> response.dateEvent() != null && response.dateEvent().isBlank())
-     *                 .filter(response -> YearMonth.from(LocalDate.parse(response.dateEvent())).equals(yearMonth))
-     *                 .onErrorResume(WebClientResponseException.TooManyRequests.class, e -> {
-     *                     log.warn("[콘텐츠 데이터 관리] 429 Too many Requests");
-     *                     return Flux.empty();
-     *                 });
-     *     }
-     *
-     *     private String circulateSoccerSeason(YearMonth yearMonth) {
-     *         int year = yearMonth.getYear();
-     *         return yearMonth.getMonthValue() >= 8 ? (year + "-" + (year + 1)) : ((year - 1) + "-" + year);
-     *     }
-     *
-     *     public Flux<TheSportResponse> getApiSport(int year, int month) {
-     *         return fluxingTheSportApi(YearMonth.of(year, month));
-     *     }
-     * }
+     * 컨텐츠 설명
      */
     @Column(nullable = false, length = 2000)
     private String description;
