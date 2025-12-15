2d6e2d62f78f8a10f7c53084e0376dbb4e1cd399
B1uffer <135810601+B1uffer@users.noreply.github.com>
2025-12-16 00:55:14 +0900

    fix(콘텐츠 데이터 관리): ContentController의 update에서 값을 정상적으로 받아오지 못하는 현상을 수정합니다.

--- src/main/java/com/codeit/playlist/domain/content/controller/ContentController.java
+++ src/main/java/com/codeit/playlist/domain/content/controller/ContentController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,7 +44,7 @@ public class ContentController {
     @PatchMapping(value = "/{contentId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ContentDto> update(@PathVariable UUID contentId,
                                              @RequestPart("request") ContentUpdateRequest request,
-                                             @RequestPart("thumbnailUrl") MultipartFile thumbnail) {
+                                             @RequestPart(value = "thumbnail", required = false) MultipartFile thumbnail) {
         ContentDto updateContents = contentService.update(contentId, request, thumbnail);
         return ResponseEntity.ok(updateContents);
     }
