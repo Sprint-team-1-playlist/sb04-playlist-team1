3e5a9ea3df25195baf7110a03b4951976398d830
B1uffer <135810601+B1uffer@users.noreply.github.com>
2025-12-15 09:32:01 +0900

    feat(콘텐츠 데이터 관리): API요청에 대한 TvSeriesListResponse와 TvSereisResponse를 추가합니다.

--- src/main/java/com/codeit/playlist/domain/content/api/response/TvSeriesListResponse.java
+++ src/main/java/com/codeit/playlist/domain/content/api/response/TvSeriesListResponse.java
@@ -1,4 +1,13 @@
 package com.codeit.playlist.domain.content.api.response;
 
-public record TvSeriesListResponse() {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public record TvSeriesListResponse(
+        @JsonProperty("results") List<TvSeriesResponse> results,
+        @JsonProperty("page") int page,
+        @JsonProperty("total_pages") int totalPages,
+        @JsonProperty("total_results") int totalResults
+) {
 }
--- src/main/java/com/codeit/playlist/domain/content/api/response/TvSeriesResponse.java
+++ src/main/java/com/codeit/playlist/domain/content/api/response/TvSeriesResponse.java
@@ -5,9 +5,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public record TvSeriesResponse(
-    @JsonProperty("results") List<TvSeriesResponse> results,
-    @JsonProperty("page") int page,
-    @JsonProperty("total_pages") int totalPages,
-    @JsonProperty("total_results") int totalResults
+        @JsonProperty("id") Long apiId,
+        @JsonProperty("title") String title,
+        @JsonProperty("overview") String description,
+        @JsonProperty("poster_path") String thumbnailUrl,
+        @JsonProperty("genre_ids") List<Integer> genreIds
 ) {
 }
