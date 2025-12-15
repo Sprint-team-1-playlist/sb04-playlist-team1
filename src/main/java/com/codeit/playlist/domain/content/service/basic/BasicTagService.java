a80699199fda857f92bb07d58082f53032171e64
B1uffer <135810601+B1uffer@users.noreply.github.com>
2025-12-15 16:05:11 +0900

    feat(콘텐츠 데이터 관리): TvSeries에 대한 태그 관리 메서드를 구현합니다.

--- src/main/java/com/codeit/playlist/domain/content/service/basic/BasicTagService.java
+++ src/main/java/com/codeit/playlist/domain/content/service/basic/BasicTagService.java
@@ -20,18 +20,39 @@ public class BasicTagService implements TagService {
     private final TmdbTagApiService tmdbTagApiService;
 
     @Override
-    public void saveTmdbTagToContent(Content content, List<Integer> genreIds) {
-        Map<Integer, String> GenreList = tmdbTagApiService.getApiMovieTag().block();
+    public void saveMovieTagToContent(Content content, List<Integer> genreIds) {
+        Map<Integer, String> genreList = tmdbTagApiService.getApiMovieTag().block();
 
-        if (GenreList == null || GenreList.isEmpty()) {
+        if (genreList == null || genreList.isEmpty()) {
             log.debug("[콘텐츠 데이터 관리] TMDB 장르 맵이 비어있습니다.");
             return;
         }
 
         for(Integer genreId : genreIds) {
-            String name = GenreList.get(genreId);
+            String name = genreList.get(genreId);
             if(name == null) {
                 log.debug("[콘텐츠 데이터 관리] genreId {}에 해당하는 장르명을 찾을 수 없습니다", genreId);
+            }
+            Tag tag = new Tag(content, name);
+            tag.setGenreId(genreId);
+            tagRepository.save(tag);
+        }
+    }
+
+    @Override
+    public void saveTvSeriesTagToContent(Content content, List<Integer> genreIds) {
+        Map<Integer, String> genreList = tmdbTagApiService.getApiTvSeriesTag().block();
+
+        if(genreList == null || genreList.isEmpty()) {
+            log.debug("[콘텐츠 데이터 관리] TMDB 장르 맵이 비어있습니다.");
+            return;
+        }
+
+        for(int i = 0; i < genreIds.size(); i++) {
+            Integer genreId = genreIds.get(i);
+            String name = genreList.get(genreId);
+            if(name == null) {
+                log.debug("[콘텐츠 데이터 관리] genreId {}에 해당하는 장르명을 찾을 수 없습니다.", genreId);
             }
             Tag tag = new Tag(content, name);
             tag.setGenreId(genreId);
