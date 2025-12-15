b8d0210d07bab9b041e72c14b931263a45b686e6
B1uffer <135810601+B1uffer@users.noreply.github.com>
2025-12-15 10:40:00 +0900

    feat(콘텐츠 데이터 관리): TvSeriesHandler를 생성합니다.

---
+++ src/main/java/com/codeit/playlist/domain/content/api/handler/TvSeriesHandler.java
@@ -0,0 +1,11 @@
+package com.codeit.playlist.domain.content.api.handler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TvSeriesHandler {
+}
