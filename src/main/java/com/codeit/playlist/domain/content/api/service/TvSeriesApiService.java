daf49c89b76f167a20e6b7329e5f2c49b0c47c17
B1uffer <135810601+B1uffer@users.noreply.github.com>
2025-12-15 10:33:05 +0900

    feat(콘텐츠 데이터 관리): TvSeriesApiService를 구현합니다.

--- src/main/java/com/codeit/playlist/domain/content/api/service/TvSeriesApiService.java
+++ src/main/java/com/codeit/playlist/domain/content/api/service/TvSeriesApiService.java
@@ -1,13 +1,16 @@
 package com.codeit.playlist.domain.content.api.service;
 
 import com.codeit.playlist.domain.content.api.response.TvSeriesListResponse;
+import com.codeit.playlist.domain.content.api.response.TvSeriesResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.retry.Retry;
 
@@ -46,5 +49,40 @@ public class TvSeriesApiService {
                 .doOnError(WebClientResponseException.class,
                         e -> log.error("[콘텐츠 데이터 관리] TvSeries API Mono {} 수집 오류 : status = {}, body = {}", query, e.getStatusCode(), e.getResponseBodyAsString())
                 );
+    }
+
+    private Flux<TvSeriesResponse> fluxingTvSeriesApi(String query, String path) {
+        log.info("[콘텐츠 데이터 관리] TvSeries Api Flux 빌드 시작");
+        return callTheTvSeriesApi(query, path, firstPage)
+                .flatMapMany(firstPageResponse -> {
+                    if(firstPageResponse.results() == null || firstPageResponse.results().isEmpty()) {
+                        return Flux.empty();
+                    }
+
+                    int totalPages = firstPageResponse.totalPages();
+                    int maxPages = Math.min(totalPages, maxPage);
+
+                    Flux<TvSeriesResponse> firstFluxResult = Flux.fromIterable(firstPageResponse.results());
+                    if(totalPages <= 1) {
+                        return firstFluxResult;
+                    }
+
+                    Flux<TvSeriesResponse> afterFluxResult = Flux.range(2, maxPages -1)
+                            .concatMap(page ->
+                                    callTheTvSeriesApi(query, path, page)
+                                            .flatMapMany(afterResponse -> {
+                                                if(afterResponse.results() == null || afterResponse.results().isEmpty()) {
+                                                    return Flux.empty();
+                                                }
+                                                return Flux.fromIterable(afterResponse.results());
+                                            })
+                            );
+                    return Flux.concat(firstFluxResult, afterFluxResult);
+                });
+    }
+
+    @Transactional
+    public Flux<TvSeriesResponse> getApiTv(String query) {
+        return fluxingTvSeriesApi(query, "/3/discover/tv");
     }
 }
