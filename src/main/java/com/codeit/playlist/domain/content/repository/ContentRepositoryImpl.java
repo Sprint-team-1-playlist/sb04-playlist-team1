64dd56e94612dfc6a96baa639d157e3024f98a48
B1uffer <135810601+B1uffer@users.noreply.github.com>
2025-12-16 10:51:48 +0900

    refactor(콘텐츠 데이터 관리): watcherCount가 swagger에 따라 long타입으로 바뀜에 따라 커서 페이지네이션에서 이를 조회하는 타입을 long value로 변경합니다.

--- src/main/java/com/codeit/playlist/domain/content/repository/ContentRepositoryImpl.java
+++ src/main/java/com/codeit/playlist/domain/content/repository/ContentRepositoryImpl.java
@@ -67,7 +67,7 @@ public class ContentRepositoryImpl implements ContentRepositoryCustom {
                     break;
 
                 case "watcherCount":
-                    long cursorWatch = Integer.valueOf(cursor);
+                    long cursorWatch = Long.parseLong(cursor);
 
                     if (ascending) {
                         builder.and(qContent.watcherCount.gt(cursorWatch)
