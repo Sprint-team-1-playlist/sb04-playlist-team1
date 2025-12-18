b5417c65bbd5e2ba837dfe60c24804c4c05a7638
B1uffer <135810601+B1uffer@users.noreply.github.com>
2025-12-18 10:41:50 +0900

    refactor(콘텐츠 데이터 관리): 테스트에서 사용하지 않는 import문을 제거합니다.

--- src/test/java/com/codeit/playlist/content/service/basic/BasicContentServiceTest.java
+++ src/test/java/com/codeit/playlist/content/service/basic/BasicContentServiceTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
